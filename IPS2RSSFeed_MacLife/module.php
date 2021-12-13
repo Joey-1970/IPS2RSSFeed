23719d7270f0db6ec3b8bc1fc32de89561a51ce0
Joey-1970 <paeper@horburg.de>
2021-12-13 13:01:50 +0100

    Update module.php

--- IPS2RSSFeed_MacLife/module.php
+++ IPS2RSSFeed_MacLife/module.php
@@ -43,7 +43,6 @@
             	// Diese Zeile nicht löschen
             	parent::ApplyChanges();
 		$this->SetStatus(102);
-		$this->RegisterMessage($this->InstanceID, 10103);
 	
 		if ($this->ReadPropertyBoolean("Active") == true) {
 			$this->GetDataUpdate();
@@ -53,16 +52,6 @@
 			$this->SetTimerInterval("Timer_1", 0);
 		}
 	}
-	
-	public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
-    	{
- 		switch ($Message) {
-			case 10103:
-				$this->ApplyChanges();
-				break;
-			
-		}
-    	}
 	    
 	// Beginn der Funktionen
 	public function GetDataUpdate()
